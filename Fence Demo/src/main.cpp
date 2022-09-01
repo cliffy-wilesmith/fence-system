9db74968070decf1c8062b08e004e2f226052167
Murabito-DN <davis.e.ngosi@gmail.com>
2022-09-01 15:59:54 +0200

    Update

--- Fence Demo/src/main.cpp
+++ Fence Demo/src/main.cpp
@@ -2,6 +2,9 @@
 #include <PinDefinitionsAndMore.h>       //Define macros for input and output pin etc.
 #include <IRremote.hpp>
 #include <CayenneLPP.h>
+#include <iostream>
+#include<string>  
+using namespace std;
 
 
        // Device Info
@@ -37,8 +40,8 @@ unsigned long debounceDelay = 200;    // the debounce time; increase if outputti
 
 long int runtime = 20000;
 const long All_clear_interval= 30*(1000);   // in seconds
-const long Alert_interval = 500;                //  in milliseconds 
-const int dt=500;
+const long Alert_interval = 5*(1000);                // seconds
+
 const int Breach_duration=3*(1000);     //in seconds
 
 
@@ -62,14 +65,19 @@ int RightAlarm_lastState = LOW;
 int LeftAlarm_currentState;
 int LeftAlarm_lastState=LOW;
 
+int BoxAlarm_currentState=LOW;
+
 int Alert_sent=LOW;
-int Val;
+int Status_code;
+int trigger;
 int count=0;
+
+CayenneLPP lpp(51);  //payloadd size
 
 //Declare Function
 
 String sendData(String command, const int timeout, boolean debug);
-void SendPayload(String msg, int DataRate);
+void SendPayload(int msg);
 void UpdateState(String Side);
 
 
@@ -85,6 +93,7 @@ void setup() {
     Serial1.begin(115200);
     SerialUSB.begin(115200);
     while (!Serial1) {; }
+    while (!SerialUSB) {; }
     SerialUSB.println("System setup");    
                          //Setup up connection
 
@@ -93,7 +102,7 @@ void setup() {
     sendData("AT+CAPPEUI=" + String(APPEUI), 200, false);    //set APPEUI
     sendData("AT+CAPPKEY=" + String(APPKEY), 200, false);   //set APPKEY
 
-    // SerialUSB.println("Connecting......");
+    SerialUSB.println("Connecting......");
     sendData("AT+CJOIN=1,0,10,12", 30000, DEBUG);           //join lorawan
     
                         // pinMode
@@ -101,12 +110,11 @@ void setup() {
     pinMode (SideSwitchPin_Right,INPUT_PULLDOWN);
     pinMode (SideSwitchPin_Left,INPUT_PULLDOWN);
 
+    SerialUSB.println("Online");
+
   }
 
 void loop() {
-
-
-
   
 
        // Reading sensor output
@@ -114,14 +122,12 @@ void loop() {
   SideSwitchVal_Right =!digitalRead(SideSwitchPin_Right);
   SideSwitchVal_Left =!digitalRead(SideSwitchPin_Left);
 
- 
+  // if( millis()-previousMillis2 >Alert_interval){
+  //       SerialUSB.println(SideSwitchVal_Right);
+  //       SerialUSB.println(SideSwitchVal_Left);
 
-  if( millis()-previousMillis2 >Alert_interval){
-        SerialUSB.println(SideSwitchVal_Right);
-        SerialUSB.println(SideSwitchVal_Left);
-
-        previousMillis2=millis();
-        }
+  //       previousMillis2=millis();
+  //       }
         
 
 if (SideSwitchVal_Right==0 || CentreSwitchVal_Right==1)             //Right side Alarm state
@@ -159,32 +165,57 @@ if (LeftAlarm_currentState == HIGH                                              
     && Alert_sent == LOW 
     && (millis()-startMillis_Left>Breach_duration ))
     {
+      trigger=13200;
       UpdateState("Alert");}
     
 if (RightAlarm_currentState == HIGH 
     && Alert_sent == LOW 
     && (millis()-startMillis_Right>Breach_duration ))
     
-    {
+    { trigger=13300;
       UpdateState("Alert");
       }
 
-if (Alert_sent==HIGH && ((millis()-previousMillis)>Alert_interval) )   //sending lora payload
+if (BoxAlarm_currentState == HIGH 
+    && Alert_sent == LOW )
+    
+    { trigger=13400;
+      UpdateState("Alert");
+      }
+
+
+ ////////////////////////////////////////////////////// //sending Alert lora payload
+if (Alert_sent==HIGH && ((millis()-previousMillis)>Alert_interval) ) 
 
 { 
-    
-        SerialUSB.println("Alert Sent");
+       int current_val=0;                           // add current fence state info to alert message
+       Status_code=trigger;
+        if(LeftAlarm_currentState==HIGH){
+          current_val+=1;
+        }
+        if(RightAlarm_currentState==HIGH){
+          current_val+=2;
+        }
 
-        SerialUSB.println(SideSwitchVal_Right);
-        SerialUSB.println(SideSwitchVal_Left);
+        if(BoxAlarm_currentState==HIGH){
+          current_val+=5;
+        }
+
+        Status_code+=current_val;
+        SerialUSB.println("Status_code:");
+        SerialUSB.println(Status_code);
+
+        SendPayload(Status_code);
+
+        current_val=0;
+
         previousMillis=millis();
         }
 
-  
-
-if (LeftAlarm_currentState == LOW && LeftAlarm_currentState == LOW &&  Alert_sent == HIGH)
+if (BoxAlarm_currentState==LOW && LeftAlarm_currentState == LOW && RightAlarm_currentState == LOW &&  Alert_sent == HIGH)
 {
   UpdateState("Alert");
+  SerialUSB.println("Sending all clear signal");
   }
 
 }
@@ -194,14 +225,14 @@ void UpdateState(String Side){
  
   if (Side=="Right"){
     if (RightAlarm_currentState==HIGH)
-    
-            {startMillis_Right=millis();}  //check right side alarm state 
+            {startMillis_Right=millis();
+            }  //check right side alarm state 
   }
    
-
   if (Side=="Left"){
     if (LeftAlarm_currentState==HIGH)
-            {startMillis_Left=millis();}  //check left side alarm state
+            {startMillis_Left=millis();
+            }  //check left side alarm state
  }
 
   if (Side=="Alert")  //check right side alarm state
@@ -211,15 +242,8 @@ void UpdateState(String Side){
               previousMillis=millis();
               
               }
-             
             }  
-
-
                           }
-
-    
-
-
 
 // Send Data Function
 
@@ -242,4 +266,25 @@ String sendData(String command, const int timeout, boolean debug)
         SerialUSB.print(response);
     }
     return response;
+}
+
+void SendPayload(int msg){
+
+    SerialUSB.println(msg);
+    lpp.reset();
+    lpp.addLuminosity(1,msg);
+    int size = lpp.getSize();
+    char payload[size];
+
+    for (int i = 0; i < size; i++) {
+        payload[i] = *(lpp.getBuffer()+i);
+    }
+    char HEXpayload[51] = "";
+
+    sprintf(HEXpayload,"AT+DTRX=1,2,%d,%02x%02x%02x%02x",size, payload[0], payload[1], payload[2], payload[3]);
+
+    SerialUSB.println("Sending........");    
+    sendData((String)HEXpayload, 4000, DEBUG);
+    
+
 }
